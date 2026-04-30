274ff0b02dd0a68561ef148deba9fb0c49277368
Arnav111garg <167680447+Arnav111garg@users.noreply.github.com>
2026-05-01 01:03:10 +0530

    Add solution for cosine-annealing-lr

---
+++ cosine-annealing-lr/cosine-annealing-lr.py
@@ -0,0 +1,7 @@
+def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
+    
+    lr = min_lr + 0.5 * (base_lr - min_lr) * (
+        1 + math.cos(math.pi * (current_step / total_steps))
+    )
+    
+    return float(lr)
