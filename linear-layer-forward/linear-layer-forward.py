2759a9d9d8e5d9200a505d8f777ec2b60f478fca
Arnav111garg <167680447+Arnav111garg@users.noreply.github.com>
2026-05-02 01:48:14 +0530

    Add solution for linear-layer-forward

---
+++ linear-layer-forward/linear-layer-forward.py
@@ -0,0 +1,16 @@
+def linear_layer_forward(X, W, b):
+    n = len(X)
+    d_in = len(X[0])
+    d_out = len(W[0])
+    
+    Y = [[0.0 for _ in range(d_out)] for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(d_out):
+            dot_product = 0.0
+            for k in range(d_in):
+                dot_product += X[i][k] * W[k][j]
+            
+            Y[i][j] = dot_product + b[j]
+            
+    return Y
